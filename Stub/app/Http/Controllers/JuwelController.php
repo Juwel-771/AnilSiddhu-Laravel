6d8b46d8155e09d7207cfa2042cf115ca004cc6d
Juwel-771 <juwelah99@gmail.com>
2022-01-18 05:43:07 +0600

    Stub

---
+++ Stub/app/Http/Controllers/JuwelController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JuwelController extends Controller
+{
+    //
+
+    public function showID($id)
+    {
+        return 'Md Juwel';
+    }
+}
